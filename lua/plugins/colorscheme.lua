c7fdd000f3d0bc6eae15648aa99dc01047e95f63
Yevgen Bushuyev <yevgen.bushuyev@gmail.com>
2024-02-21 08:46:12 +0100

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  --  {
+  --    "navarasu/onedark.nvim",
+  --    priority = 1000,
+  --    opts = {
+  --      style = "dark",
+  --    },
+  --  },
+  {
+    "LazyVim/LazyVim",
+    priority = 1000,
+    opts = {
+      colorscheme = "habamax",
+    },
+  },
+}
